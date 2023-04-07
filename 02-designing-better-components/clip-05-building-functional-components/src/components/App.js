d55ed8c584b736d6437a0072742cdb6f01c60cb9
Willard Ward <willardwardiii@gmail.com>
2023-04-07 16:28:19 -0500

    added state hooks for the light and dark themes

---
+++ 02-designing-better-components/clip-05-building-functional-components/src/components/App.js
@@ -0,0 +1,22 @@
+import { data } from "../../SpeakerData";
+import SpeakersList from "./SpeakersList";
+import Header from "./Header";
+import SpeakersToolbar from "./SpeakersToolbar";
+import { useState } from "react";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+  return (
+    <div
+      className={
+        theme === "light" ? "container-fluid light" : "container-fluid dark"
+      }
+    >
+      <Header theme={theme} />
+      <SpeakersToolbar theme={theme} setTheme={setTheme} />
+      <SpeakersList data={data} />
+    </div>
+  );
+}
+
+export default App;
