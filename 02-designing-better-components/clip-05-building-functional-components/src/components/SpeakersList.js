9ea2356de60f521ecb20ec125a6dab2eeaa5ebf2
Willard Ward <willardwardiii@gmail.com>
2023-04-08 10:17:05 -0500

    added hook file and moved appropriate functions into it

--- 02-designing-better-components/clip-05-building-functional-components/src/components/SpeakersList.js
+++ 02-designing-better-components/clip-05-building-functional-components/src/components/SpeakersList.js
@@ -1,46 +1,10 @@
 import Speaker from "./Speaker";
-import { data } from "../../SpeakerData";
-import { useState, useEffect } from "react";
 import ReactPlaceHolder from "react-placeholder";
+import useRequestSpeakers from "../hooks/useRequestSpeakers";
 
 function SpeakersList({ showSessions }) {
-  const [speakersData, setSpeakersData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasErrored, setHasErrored] = useState(false);
-  const [error, setError] = useState("");
-
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, null));
-
-  useEffect(() => {
-    async function delayFunc() {
-      try {
-        await delay(2000);
-        // throw "Had Error.";
-        setIsLoading(false);
-        setSpeakersData(data);
-      } catch (e) {
-        setIsLoading(false);
-        setHasErrored(true);
-        setError(e);
-      }
-    }
-    delayFunc();
-  }, []);
-
-  function onFavoriteToggle(id) {
-    const speakersRecPrevious = speakersData.find(function (rec) {
-      return rec.id === id;
-    });
-    const speakerRecUpdated = {
-      ...speakersRecPrevious,
-      favorite: !speakersRecPrevious.favorite,
-    };
-    const speakersDataNew = speakersData.map(function (rec) {
-      return rec.id === id ? speakerRecUpdated : rec;
-    });
-
-    setSpeakersData(speakersDataNew);
-  }
+  const { speakersData, isLoading, hasErrored, error, onFavoriteToggle } =
+    useRequestSpeakers(2000);
 
   if (hasErrored === true) {
     return (
@@ -50,11 +14,16 @@ function SpeakersList({ showSessions }) {
     );
   }
 
-  if (isLoading === true) return <div>Loading...</div>;
+  // if (isLoading === true) return <div>Loading...</div>;
 
   return (
     <div className="container speakers-list">
-      <ReactPlaceHolder>
+      <ReactPlaceHolder
+        type="media"
+        rows={15}
+        className="speakersList-placeholder"
+        ready={isLoading === false}
+      >
         <div className="row">
           {speakersData.map(function (speaker) {
             return (
