39b191404db2e09cddc3dd08e6d4d620a554e200
Willard Ward <willardwardiii@gmail.com>
2023-04-10 20:38:52 -0500

    showed example of RenderProps with the /speakers webpage

--- 02-designing-better-components/clip-05-building-functional-components/src/components/withData.js
+++ 02-designing-better-components/clip-05-building-functional-components/src/components/withData.js
@@ -1,11 +1,14 @@
-function withData(Component) {
-  const speakers = [
-    { imageSrc: "speaker-1124", name: "Douglas Crockford" },
-    { imageSrc: "speaker-1530", name: "Tamara Baker" },
-    { imageSrc: "speaker-10803", name: "Eugene Chuvyrov" },
-  ];
-  return function () {
-    return <Component speakers={speakers}></Component>;
+function withData(maxSpeakersToShow) {
+  return function (Component) {
+    const speakers = [
+      { imageSrc: "speaker-1124", name: "Douglas Crockford" },
+      { imageSrc: "speaker-1530", name: "Tamara Baker" },
+      { imageSrc: "speaker-10803", name: "Eugene Chuvyrov" },
+    ];
+    return function () {
+      const limitSpeakers = speakers.slice(0, maxSpeakersToShow);
+      return <Component speakers={limitSpeakers}></Component>;
+    };
   };
 }
 
